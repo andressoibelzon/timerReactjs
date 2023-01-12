8f5ceb22a92a7a6b7ef721406b286fc263455f1a
Andres Soibelzon <a.soibelzon@gmail.com>
2023-01-12 20:27:13 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container py-3 my-5 d-flex justify-content-center bg-dark text-white display-5 font-weight-bold">
+			<div className="px-3">{props.hora} </div>
+			<div className="px-3">{props.minuto}</div> 
+			<div className="px-3">{props.desena}</div>
+			<div className="px-3">{props.segundo}</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,30 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let segundo = 0;
+let desena = 0;
+let minuto = 0;
+let hora = 0;
+
+setInterval(function() {
+    console.log("funciona");
+    segundo++
+    if (segundo === 10) {
+        desena++
+        segundo = 0
+    } 
+    if (desena === 6) {
+        minuto++
+        desena = 0
+    }
+    if (minuto === 60) {
+        hora++
+        minuto = 0
+    }
+
+
+
+    ReactDOM.render(<Home segundo={segundo} desena={desena} minuto={minuto} hora={hora}/>, document.querySelector("#app"));
+}, 1000)
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
